82e0a12cff0625d0e9b8fe0c79addcf3f7db1a6a
이유현 <79284807+YouHyun-Lee@users.noreply.github.com>
2021-02-22 06:30:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const something = "Something"
-
-const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", 54, true, "stuff", "lalala", something];
-
-console.log(daysOfWeek);
+const nicoInfo = {
+  name: "Nico",
+  age: 33,
+  gender:"Male",
+  isHandsome: true
+  favMovies: ["Along the gods", "LOTR", "Oldboy"],
+  favFood: [
+    {
+      name: "Kimchi",
+      fatty: false
+     },
+     {
+       name:"Cheese burger",
+       fatty: true
+     }
+  ]
+}
+console.log(nicoInfo);
