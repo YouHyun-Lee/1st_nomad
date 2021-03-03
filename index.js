9800e36f3559e2bf2e435ceda2fb8dc358946038
이유현 <79284807+YouHyun-Lee@users.noreply.github.com>
2021-03-04 08:02:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-consost title = document.getElementById("title");
+consost title = document.querySelector(".title");
 title.innerHTML="Hi! From JS";
 title.style.color = "red";
 document.tile = "I own you now";
