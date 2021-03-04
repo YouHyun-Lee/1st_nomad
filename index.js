3cebb3bcec6663a042ed38c11c79bf724700d49d
이유현 <79284807+YouHyun-Lee@users.noreply.github.com>
2021-03-05 06:52:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-if ("10" === 10){
-  console.log("hi")
-} else if ("10" === "10"){
-  console.log("lalala");
-} else{
-  console.log("ho");
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d"
+
+function handleClick() {
+  const currentColor = title.style.color;
+  console.log(title.style.color);
 }
+
+function init(){
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init();
