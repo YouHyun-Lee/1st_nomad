03eb15decb54d3d1b43ae1c0093b9cf46d4ddd92
이유현 <79284807+YouHyun-Lee@users.noreply.github.com>
2021-03-05 07:01:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const OTHER_COLOR = "#7f8c8d"
 
 function handleClick() {
   const currentColor = title.style.color;
-  console.log(title.style.color);
+  if (currentColor === BASE_COLOR){
+    title.style.color = OTHER_COLOR;
+  } else{
+    title.style.color = BASE_COLOR;
+  }
 }
 
 function init(){
